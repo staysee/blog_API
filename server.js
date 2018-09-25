66c1bbeff2fb87784aec8203a62794540fe34aa2
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-24 19:23:08 -0700

    created POST endpoint

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ app.get('/blogposts', (req, res) =>{
   res.json(BlogPosts.get());
 })
 
+app.post('/blogposts', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+
+  for (let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    if (!(field in req.body)){
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(item);
+});
 
  app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
